897539198b9ad4907c9b16f03ac352c14c36af15
Ariunsanaa <luftballons2010@gmail.com>
2019-03-27 09:43:25 +0800

    Updates

---
+++ precache-manifest.bf6f130a5cd3b24f34e0bcd28efc8681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "6850da7bfe3aaec067a4",
+    "url": "/contactmanager/static/js/main.98616e7f.chunk.js"
+  },
+  {
+    "revision": "cf255210dd8130c7c3c8",
+    "url": "/contactmanager/static/js/2.8b56081d.chunk.js"
+  },
+  {
+    "revision": "6850da7bfe3aaec067a4",
+    "url": "/contactmanager/static/css/main.051cb929.chunk.css"
+  },
+  {
+    "revision": "cf255210dd8130c7c3c8",
+    "url": "/contactmanager/static/css/2.9add5ba8.chunk.css"
+  },
+  {
+    "revision": "301c7477cb7542f3cbecc62bdbe30fd3",
+    "url": "/contactmanager/index.html"
+  }
+];
